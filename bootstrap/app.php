9655824c4a8be88825b7207a3da9ce80a93b1437
Aleksei Udodov <weberdster@gmail.com>
2025-10-16 16:44:03 +0400

    custom error message on 404

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Middleware\ApiKeyAuth;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +18,15 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (ModelNotFoundException|NotFoundHttpException $e, $request) {
+            // Only return simple message if debug is disabled
+            if (!config('app.debug')) {
+                return response()->json([
+                    'message' => 'Resource not found',
+                ], 404);
+            }
+
+            // Return null to let Laravel fallback to the default error handler in debug mode
+            return null;
+        });
     })->create();
