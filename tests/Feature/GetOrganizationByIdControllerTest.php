f0c051e0f50fa98b96259645cb32a695596077b0
Aleksei Udodov <weberdster@gmail.com>
2025-10-19 15:11:32 +0400

    added feature test

---
+++ tests/Feature/GetOrganizationByIdControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Organization;
+use Illuminate\Testing\Fluent\AssertableJson;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class GetOrganizationByIdControllerTest extends TestCase
+{
+    #[Test]
+    public function it_returns_organization_by_id_successfully()
+    {
+        $organization = Organization::factory()->create();
+        $response = $this->withHeaders([
+            'API-KEY' => config('app.api_key'),
+            'Accept' => 'application/json',
+        ])->getJson("/api/v1/organizations/{$organization->id}");
+
+        $response
+            ->assertOk()
+            ->assertJson(fn (AssertableJson $json) =>
+            $json->where('data.id', $organization->id)
+                ->where('data.name', $organization->name)
+                ->etc()
+            );
+    }
+
+    #[Test]
+    public function it_returns_404_if_organization_not_found()
+    {
+        $nonExistingId = 999999;
+
+        $response = $this->withHeaders([
+            'API-KEY' => config('app.api_key'),
+            'Accept' => 'application/json',
+        ])->getJson("/api/v1/organizations/{$nonExistingId}");
+
+        // Assert
+        $response
+            ->assertNotFound()
+            ->assertJson(fn (AssertableJson $json) =>
+            $json->where('message', 'Resource not found')->etc()
+            );
+    }
+
+    #[Test]
+    public function it_returns_422_if_id_is_invalid()
+    {
+        // Act
+        $response = $this->withHeaders([
+            'API-KEY' => config('app.api_key'),
+            'Accept' => 'application/json',
+        ])->getJson('/api/v1/organizations/invalid-id');
+
+        // Assert
+        $response
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['id']);
+    }
+}
